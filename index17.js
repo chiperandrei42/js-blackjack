9713806cb764229450658533f0713dada23d7e1a
Chiper Andrei <138249161+chiperandrei42@users.noreply.github.com>
2024-08-26 22:03:56 +0300

    added code to simulate suits

--- index17.js
+++ index17.js
@@ -21,7 +21,19 @@ const cardsObj = {
     'A': 11
 };
 
-const cards = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"];
+const cards = ["A", "A", "A", "A",
+    2, 2, 2, 2,
+    3, 3, 3, 3,
+    4, 4, 4, 4,
+    5, 5, 5, 5,
+    6, 6, 6, 6,
+    7, 7, 7, 7,
+    8, 8, 8, 8,
+    9, 9, 9, 9,
+    10, 10, 10, 10,
+    "J", "J", "J", "J",
+    "Q", "Q", "Q", "Q",
+    "K", "K", "K", "K"];
 
 fisherYates(cards);
 
@@ -31,6 +43,7 @@ function fisherYates(array) {
         [array[i], array[random]] = [array[random], array[i]];
     }
 }
+console.log(cards);
 
 const playerCards = [cards.shift(), cards.shift()];
 const dealerCards = [cards.shift()];
